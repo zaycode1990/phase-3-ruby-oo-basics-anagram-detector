ea9f1c3aef316e0eff0c0c0a9549a874f7ebaafb
Isaiah <ijandicos90@gmail.com>
2022-07-19 01:28:18 -0400

    commit tree 79797cf7f4e78c410dc99df18560cb9d66be21f1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_writer :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_arr)
+    my_tired_array = []
+    words_arr.each{|word_of_arr|  word_of_arr.chars.sort == @word.chars.sort ? my_tired_array.push(word_of_arr) : []}
+    my_tired_array
+  end
+end
